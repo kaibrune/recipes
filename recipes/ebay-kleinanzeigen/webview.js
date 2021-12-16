cdf998b886632c35c98bcaf29b272e64a8469e3b
Kai Brune <mail@brune.io>
2021-12-16 15:01:14 +0100

    added ebay-kleinanzeigen recipe

---
+++ recipes/ebay-kleinanzeigen/webview.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const path = require('path');
+module.exports = (Ferdi) => {
+  const getMessages = () => {
+    
+    var output=0;
+    $('.ConversationList').find('.ConversationListItem--Counter').each(function () {
+        output += parseInt(this.innerHTML);
+    });
+    const directMessages = output;
+
+    Ferdi.setBadge(directMessages);
+  }
+
+  Ferdi.loop(getMessages);
+
+  window.addEventListener("load", function(){
+    var breadcrump = document.getElementById("vap-brdcrmb");
+    if(breadcrump!=null){
+      breadcrump.innerHTML ='<a class="back-button" href="/m-nachrichten.html"><span class="Button--Body"><span>Return to messenger</span></span></a>' + breadcrump.innerHTML;
+    }
+
+    var error_msg = document.getElementById("my-manageads-content");
+    if(error_msg!=null){
+    error_msg.innerHTML ='<a class="back-button" href="/m-nachrichten.html"><span class="Button--Body"><span>Return to messenger</span></span></a>' + error_msg.innerHTML;
+    }
+  });
+  
+  Ferdi.injectCSS(path.join(__dirname, 'style.css'));
+}
